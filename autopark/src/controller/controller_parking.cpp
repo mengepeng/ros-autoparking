99ea508357961ad6443428b011e328803121943d
mengepeng <mengepeng@outlook.com>
2020-05-18 23:56:35 +0200

    #CHANGE: set move speed for search parking space

--- autopark/src/controller/controller_parking.cpp
+++ autopark/src/controller/controller_parking.cpp
@@ -4,34 +4,42 @@
  * Author: Meng Peng
  * Date: 2020-04-15
  * Description: publish message to the topic parking_enable to 
- * start or stop search parking space
+ * start search parking space
  * 
  ******************************************************************/
 
-#include <sstream>
 #include <ros/ros.h>
 #include <std_msgs/Bool.h>
+#include <std_msgs/Float32.h>
+#include "autopark/autoparking.h"
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "controller_parking");
     ros::NodeHandle nh;
-    ros::Publisher pub = nh.advertise<std_msgs::Bool>("parking_enable", 1);
+    ros::Publisher pub_enable = nh.advertise<std_msgs::Bool>("parking_enable", 1);
+    ros::Publisher pub_move = nh.advertise<std_msgs::Bool>("cmd_move", 1);
+
     ros::Duration(1).sleep();   // wait 1 second for creating communication
 
     // define message
     std_msgs::Bool msg_parking_enable;
+    std_msgs::Float32 msg_move;
     // set message
     msg_parking_enable.data = true;
+    msg_move.data = speed_search_parking;
 
     while (ros::ok())
     {
-        if (pub.getNumSubscribers() > 0)
+        if (pub_enable.getNumSubscribers() > 0 && pub_move.getNumSubscribers() > 0)
         {
             // output the published message
             ROS_INFO("parking enable: %d", msg_parking_enable.data);
+            ROS_INFO("cmd_move: %f", msg_move.data);
+
             // publish message 
-            pub.publish(msg_parking_enable);
+            pub_enable.publish(msg_parking_enable);
+            pub_enable.publish(msg_move);
 
             ros::spinOnce();
 
